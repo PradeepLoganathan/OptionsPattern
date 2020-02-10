cd911ed1dc70782302fb2cd004f3c7621e1ec5b3
Pradeep Loganathan <pradeep@pradeeploganathan.com>
2020-02-10 20:10:26 +1000

    Add project files.

---
+++ OptionsPattern/Program.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using OptionsPattern.configuration;
+using System;
+using System.IO;
+
+namespace OptionsPattern
+{
+    class Program
+    {
+        private static IServiceProvider _serviceProvider;
+
+        static void Main(string[] args)
+        {
+            IConfiguration Configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .AddCommandLine(args)
+                .Build();
+
+            RegisterServices(Configuration);
+
+            DisposeServices();
+        }
+
+        private static void RegisterServices(IConfiguration configuration)
+        {
+            var services = new ServiceCollection();
+            services.AddOptions();
+            services.Configure<LoggingConfig>(configuration.GetSection(""));
+            
+            _serviceProvider = services.BuildServiceProvider(true);
+        }
+
+        private static void DisposeServices()
+        {
+            if (_serviceProvider == null)
+            {
+                return;
+            }
+            if (_serviceProvider is IDisposable)
+            {
+                ((IDisposable)_serviceProvider).Dispose();
+            }
+        }
+    }
+}
---
+++ OptionsPattern/configuration/LoggingConfig.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OptionsPattern.configuration
+{
+    public class LoggingConfig
+    {
+        public string LoggingLevel { get; set; }
+        public string LogfilePath { get; set; }
+    }
+}
